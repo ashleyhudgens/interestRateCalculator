3702e4c1906bf263f75788b359ec61fe8cd30415
ashleyhudgens <39204522+ashleyhudgens@users.noreply.github.com>
2018-06-05 19:40:24 -0500

    Add files via upload
    Converted loops to methods to simplify code

--- interestRateCalculator.java
+++ interestRateCalculator.java
@@ -1,78 +1,95 @@
+//import modules
 import static java.lang.System.out;
 import java.util.Scanner;
 
-public class interestRateCalculator {
-    public static void main(String[] args) {
+public class interestRateCalculator
+{
+    public static void main(String[] args)
+    {
+        //declare variables
         Scanner keyboard = new Scanner(System.in);
-        int numberOfPeople;
-        int numberOfWallets;
-        int numberOfCards;
         String cardBrand;
+        double cardInterestRate;
+        double cardInterest;
         double cardBalance;
-        double mcInterestRate = .05;
-        double visaInterestRate = .1;
-        double discoverInterestRate = .01;
-        double mcCardInterestRate;
-        double visaCardInterestRate;
-        double discoverCardInterestRate;
-        double walletInterestRate = 0;
-        double personInterestRate = 0;
+        int numberOfCards;
+        int numberOfWallets;
+        double walletTotal = 0;
+        int numberOfPeople;
+        double perPersonInterest = 0;
 
         out.println("Interest Rate Calculator");
         out.println();
-        out.println("How many people? ");
+
+        //run through each person to get total interest rate per card, per wallet and per person
+        out.print("How many people? ");
         numberOfPeople = keyboard.nextInt();
-        while (numberOfPeople != 0) {
-            out.print("How many wallets does this person have? ");
+
+        while (numberOfPeople != 0)
+        {
+            out.print("How many wallets? ");
             numberOfWallets = keyboard.nextInt();
-            numberOfPeople--;
 
-            while (numberOfWallets != 0) {
-                out.println("How many cards? ");
+            while (numberOfWallets != 0)
+            {
+                out.print("How many cards? ");
                 numberOfCards = keyboard.nextInt();
-                numberOfWallets--;
-                while (numberOfCards != 0) {
 
-                    out.println("what is the card brand?");
+                while (numberOfCards != 0)
+                {
+                    out.print("What is the card brand? ");
                     cardBrand = keyboard.next();
 
-                    switch (cardBrand) {
-                        case "Mastercard":
-                            out.println("what is the card balance?");
-                            cardBalance = keyboard.nextDouble();
-                            mcCardInterestRate = cardBalance * mcInterestRate;
-                            out.println("The total interest rate for this card is " + mcCardInterestRate);
-                            walletInterestRate += mcCardInterestRate;
-                            personInterestRate += walletInterestRate;
-                            numberOfCards--;
-                            break;
-                        case "Visa":
-                            out.println("what is the card balance?");
-                            cardBalance = keyboard.nextDouble();
-                            visaCardInterestRate = cardBalance * visaInterestRate;
-                            out.println("The total interest rate for this card is " + visaCardInterestRate);
-                            walletInterestRate += visaCardInterestRate;
-                            personInterestRate += walletInterestRate;
-                            numberOfCards--;
-                            break;
-                        case "Discover":
-                            out.println("what is the card balance?");
-                            cardBalance = keyboard.nextDouble();
-                            discoverCardInterestRate = cardBalance * discoverInterestRate;
-                            out.println("The total interest rate for this card is " + discoverCardInterestRate);
-                            walletInterestRate += discoverCardInterestRate;
-                            personInterestRate += walletInterestRate;
-                            numberOfCards--;
-                            break;
-                        default:
-                            out.println("Invalid input");
-                            break;
-                    }
+                    cardInterestRate = getInterestRate(cardBrand);
+
+                    out.print("What is the balance on the card? ");
+                    cardBalance = keyboard.nextDouble();
+
+                    cardInterest = getCardInterest(cardInterestRate, cardBalance);
+                    walletTotal += cardInterest;
+                    perPersonInterest += walletTotal;
+
+                    out.println("The total interest for this " + cardBrand + " is " + cardInterest);
+                    numberOfCards--;
                 }
-                out.println("The total interest rate for this wallet is " + walletInterestRate);
+                out.println("The total interest for this wallet is " + walletTotal);
+                numberOfWallets--;
             }
-            out.println("The total interest rate for this person is " + personInterestRate);
+            out.println("The total interest for this person is " + perPersonInterest);
+            numberOfPeople--;
         }
+
     }
+
+    private static double getCardInterest (double cardInterestRate, double cardBalance)
+    {
+        //calculates total interest per card depending on the current balance on the card and current interest rate
+        return cardInterestRate * cardBalance;
+    }
+
+
+    private static double getInterestRate (String cardBrand)
+    {
+        //determines current interest rate according to card brand
+        double interestRate = 0;
+
+        switch (cardBrand)
+        {
+            case "Mastercard":
+                interestRate = .05;
+                break;
+            case "Visa":
+                interestRate = .1;
+                break;
+            case "Discover":
+                interestRate = .01;
+                break;
+            default:
+                out.println("Invalid input");
+                break;
+        }
+       return interestRate;
+    }
+
 }
 
