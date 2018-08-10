95c6b4db9eb08c30b8c45724fa76c877f6829418
ashleyhudgens <39204522+ashleyhudgens@users.noreply.github.com>
2018-08-10 08:50:07 -0500

    Update interestRateCalculatorTest.java

--- interestRateCalculatorTest.java
+++ interestRateCalculatorTest.java
@@ -2,62 +2,72 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class interestRateCalculatorTest {
+public class interestRateCalculatorTest 
+{
 
     @Test
-    public void cardBalanceTest() throws Exception{
+    public void cardBalanceTest() throws Exception
+    {
         double balance = interestRateCalculator.cardBalance(100);
         assertEquals(100, balance, 0);
     }
 
 
     @Test
-    public void calculateWalletInterestCaseThreeSecondPersonWalletTest() throws Exception{
+    public void calculateWalletInterestCaseThreeSecondPersonWalletTest() throws Exception
+    {
         double caseThreeWalletSecondPerson = interestRateCalculator.calculateWalletInterestCaseThreeSecondPersonWallet(1);
         assertEquals(15, caseThreeWalletSecondPerson, 0);
     }
 
     @Test
-    public void calculateWalletInterestCaseThreeFirstPersonWalletTest() throws Exception {
+    public void calculateWalletInterestCaseThreeFirstPersonWalletTest() throws Exception 
+    {
         double caseThreeFirstPerson = interestRateCalculator.calculateWalletInterestCaseThreeFirstPersonWallet(1);
         assertEquals(15, caseThreeFirstPerson, 0);
     }
 
     @Test
-    public void calculateWalletInterestCaseTwoWalletTwoTest() throws Exception {
+    public void calculateWalletInterestCaseTwoWalletTwoTest() throws Exception 
+    {
         double caseTwoWalletTwo = interestRateCalculator.calculateWalletInterestCaseTwoWalletTwo(1);
         assertEquals(5, caseTwoWalletTwo, 0);
 
     }
 
     @Test
-    public void calculateWalletInterestCaseTwoWalletOneTest() throws Exception{
+    public void calculateWalletInterestCaseTwoWalletOneTest() throws Exception
+    {
         double caseTwoWalletOne = interestRateCalculator.calculateWalletInterestCaseTwoWalletOne(1);
         assertEquals(11, caseTwoWalletOne, 0);
 
     }
 
     @Test
-    public void calculateWalletInterestCaseOneTest() throws Exception{
+    public void calculateWalletInterestCaseOneTest() throws Exception
+    {
         double walletInterest = interestRateCalculator.calculateWalletInterestCaseOne(1);
         assertEquals(16.0, walletInterest,0);
 
     }
 
     @Test
-    public void calculateMasterCardInterestTest() throws Exception{
+    public void calculateMasterCardInterestTest() throws Exception
+    {
         Double cardInterest = interestRateCalculator.calculateCardInterest("MasterCard");
         assertEquals(5, cardInterest.intValue());
     }
 
     @Test
-    public void calculateDiscoverInterestTest() throws Exception{
+    public void calculateDiscoverInterestTest() throws Exception
+    {
         Double cardInterest = interestRateCalculator.calculateCardInterest("Discover");
         assertEquals(1, cardInterest.intValue());
     }
 
     @Test
-    public void calculateVisaInterestTest() throws Exception{
+    public void calculateVisaInterestTest() throws Exception
+    {
         Double cardInterest = interestRateCalculator.calculateCardInterest("Visa");
         assertEquals(10, cardInterest.intValue());
     }
